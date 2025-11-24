a8d78b886471fa56208691be60e5016b28f6d9f4
Abdulrehman Seyal <abdulrehmanseyal4@gmail.com>
2025-11-24 22:07:50 +0500

    Chat history

--- backend/core.py
+++ backend/core.py
@@ -4,13 +4,15 @@ from langchain_classic.chains.combine_documents.stuff import create_stuff_docume
 from langchain_pinecone import PineconeVectorStore
 from langchain_ollama import OllamaEmbeddings, ChatOllama
 from langsmith import Client
+from typing import List, Dict, Any
+from langchain_classic.chains.history_aware_retriever import create_history_aware_retriever
 
 load_dotenv()
 
 INDEX_NAME="langchain-doc-index"
 client = Client()
 
-def run_llm(query: str):
+def run_llm(query: str, chat_history: List[Dict[str, Any]]):
     embeddings = OllamaEmbeddings(model="mxbai-embed-large:latest")
     docsearch = PineconeVectorStore(index_name=INDEX_NAME, embedding=embeddings)
     chat = ChatOllama(model="llama3.2:latest", verbose=True)
@@ -18,11 +20,16 @@ def run_llm(query: str):
     retrieval_qa_chat_prompt = client.pull_prompt("langchain-ai/retrieval-qa-chat")
     stuff_documents_chain = create_stuff_documents_chain(llm=chat, prompt=retrieval_qa_chat_prompt)
 
-    qa = create_retrieval_chain(
-        retriever=docsearch.as_retriever(), combine_docs_chain=stuff_documents_chain
+    rephrase_prompt = client.pull_prompt("langchain-ai/chat-langchain-rephrase")
+    history_aware_retriever = create_history_aware_retriever(
+        llm=chat, retriever=docsearch.as_retriever(), prompt=rephrase_prompt
     )
 
-    result = qa.invoke(input={"input": query})
+    qa = create_retrieval_chain(
+        retriever=history_aware_retriever, combine_docs_chain=stuff_documents_chain
+    )
+
+    result = qa.invoke(input={"input": query, "chat_history": chat_history})
     new_result = {
         "query": result["input"],
         "result": result["answer"],
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ load_dotenv()
 from backend.core import run_llm
 import streamlit as st
 
+
 def create_sources_string(sources_urls: set[str]) -> str:
     if not sources_urls:
         return ""
@@ -15,34 +16,45 @@ def create_sources_string(sources_urls: set[str]) -> str:
         sources_string += f"{i+1}. {sources}\n"
     return sources_string
 
+
 def main():
     print("Hello from documentation-helper!")
     st.header("Documentation Helper")
 
     prompt = st.text_input("Where should we begin?", placeholder="Enter your prompt")
-    if "user_prompt_history" not in st.session_state:
-        st.session_state["user_prompt_history"] = []
 
-    if "chat_answers_history" not in st.session_state:
+    if (
+        "chat_answers_history" not in st.session_state
+        and "user_prompt_history" not in st.session_state
+        and "chat_history" not in st.session_state
+    ):
         st.session_state["chat_answers_history"] = []
+        st.session_state["user_prompt_history"] = []
+        st.session_state["chat_history"] = []
 
     if prompt:
         with st.spinner("Generating Response.."):
-            generated_response = run_llm(query=prompt)
-            sources = set(doc.metadata["source"] for doc in generated_response["source_documents"])
-            
+            generated_response = run_llm(query=prompt, chat_history=st.session_state["chat_history"])
+            sources = set(
+                doc.metadata["source"] for doc in generated_response["source_documents"]
+            )
+
             formatted_response = (
                 f"{generated_response["result"]} \n\n {create_sources_string(sources)}"
             )
 
             st.session_state["user_prompt_history"].append(prompt)
             st.session_state["chat_answers_history"].append(formatted_response)
+            st.session_state["chat_history"].append(("human", prompt))
+            st.session_state["chat_history"].append(("ai", generated_response["result"]))
 
     if st.session_state["chat_answers_history"]:
-        for generated_response, user_query in zip(st.session_state["chat_answers_history"], st.session_state["user_prompt_history"]):
+        for generated_response, user_query in zip(
+            st.session_state["chat_answers_history"],
+            st.session_state["user_prompt_history"],
+        ):
             st.chat_message("user").write(user_query)
             st.chat_message("assistant").write(generated_response)
-
 
 
 if __name__ == "__main__":
